30093823fafff95680b83f9eca0caa9a47aa1178
Hector Orellana <pulga.p.29@gmail.com>
2015-08-20 02:59:54 -0300

    Algunos procedimientos nuevos.
    Mas que nada es para facilitar el uso de la base de datos
    desde el script en python.

--- dolar.sql
+++ dolar.sql
@@ -8,3 +8,29 @@ create table Dolar
 ( idDolar int not null auto_increment primary key,
   Fecha date not null,
   Valor float not null );
+
+create procedure buscar_fecha(fecha date) 
+	select *
+    from Dolar d
+    where d.Fecha = fecha;
+
+
+-- `delimiter` cambia el caracter para terminar una sentencia,
+-- en este caso lo cambio a `//`
+
+delimiter //
+create procedure agregar_fecha(in fecha date, in valor float)
+begin
+  insert into Dolar (Fecha, Valor) values (fecha, valor);
+end //
+delimiter ;
+
+
+delimiter //
+create procedure actualizar_fecha(in fecha date, in nuevo_valor float)
+begin
+	update Dolar
+    set Valor = nuevo_valor
+    where Dolar.Fecha = fecha;
+end //
+delimiter ;
